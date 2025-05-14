b27b097eab550d1fab42ed70310fc5b6ce2a479e
ravidu_senevirathne <senevirathneravidu@gmail.com>
2025-05-14 19:19:16 +0530

    initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Blog-Nest') }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-xl">Hello</h1>
+    <p class="font-weight-bolder">Hey</p>
+</body>
+</html>
